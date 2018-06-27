f7b6c9a80dfb9898cab9cf638d5e84eac5601df9
Martijn van der Veen <turiphro@gmail.com>
2018-06-26 21:38:34 -0700

    Global gitignore; submodule fix for recursion

--- install.sh
+++ install.sh
@@ -5,16 +5,16 @@ DOTFILES=$(dirname $(realpath $BASH_SOURCE))
 # TODO function that checks if it exists (backup or ask)
 
 # update all submodules
-git submodule update --init --recursive
-git submodule update --recursive --remote
+git submodule update --init --recursive # checkout all repos as-is
+#git submodule update --remote # update top-level only (no --recursive)
 
 
 # add symlinks to dotfiles in this repo
-for FILE in .zshrc .vimrc .tmux.conf .gitconfig; do
+for FILE in .zshrc .vimrc .tmux.conf .gitconfig .gitignore; do
     ln -s $DOTFILES/$FILE $HOME/$FILE
 done
 
-for DIR in .oh-my-zsh/ .vim/; do
+for DIR in .oh-my-zsh .vim; do
     ln -s $DOTFILES/$DIR $HOME/
 done
 
